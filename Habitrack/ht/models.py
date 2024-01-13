886670ab8e26cb17b828497628a730d7a662a8fa
Braden <bradenoptimus@gmail.com>
2024-01-13 07:41:05 -0700

    may have broken things a bit

--- Habitrack/ht/models.py
+++ Habitrack/ht/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+# So, for the database side of things, I think that we'll have a bunch of 
+# habits, and then entries which correspond to them. When on the calendar page, 
+# we'll simply get a list of all of the different entries, and populate a list
+# of all of the calendar days, and we'll initially populate them with a blank
+# value, and then update all of them if they actually have been entered in. So,
+# in essence, our backend will actually be a backend, and not just a query 
+# logger haha.
+
+
+class BinHabit(models.Model):
+    name = models.CharField(max_length=5)
+    goal = models.FloatField()
+    start = models.DateField()
+    stop = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class BinaryEntry(models.Model):
+    habit = models.ForeignKey(BinHabit, on_delete=models.CASCADE)
+    res = models.BooleanField()
+    date = models.DateField()
+    
+    def __str__(self):
+        return str(self.date) + BinHabit.objects.filter(id=self.habit).name
+
+class NumHabit(models.Model):
+    name = models.CharField(max_length=5)
+    goal = models.FloatField()
+    start = models.DateField()
+    stop = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class NumEntry(models.Model):
+    habit = models.ForeignKey(NumHabit, on_delete=models.CASCADE)
+    res = models.FloatField()
+    date = models.DateField()
+
+    
+    def __str__(self):
+        return str(self.date) + NumHabit.objects.filter(id=self.habit).name
--- Habitrack/ht/views.py
+++ Habitrack/ht/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 
+# Let's start  this from the top, shall we? When I start  the app, I want it to 
+# generate a list of binary and numeric habits. I think that the easiest way to
+# do  this would simply be to  currently have  it query all binary and  numeric 
+# tasks.  Then I could make a  couple different pages. I  think there will be a 
+# calendar  page, a  summary page, and each of  these will have the  ability to 
+# have  an  individual habit selected  or to select  all habits. This  gives us 
+# routes for /summary/<id>, /summary/all, /calendar/<id>, and /calendar/all. 
+
 def index(request):
 	return HttpResponse("Hello butt")
