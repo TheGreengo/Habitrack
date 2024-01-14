2f6fcff8917b25a1444c5ed43e7351d2e470bab0
Braden <bradenoptimus@gmail.com>
2024-01-14 15:10:09 -0700

    I think this will work?

--- Habitrack/ht/views.py
+++ Habitrack/ht/views.py
@@ -8,7 +8,11 @@ from .models import BinEntry, BinHabit, NumEntry, NumHabit
 # tasks.  Then I could make a  couple different pages. I  think there will be a 
 # calendar  page, a  summary page, and each of  these will have the  ability to 
 # have  an  individual habit selected  or to select  all habits. This  gives us 
-# routes for /summary/<id>, /summary/all, /calendar/<id>, and /calendar/all. 
+# routes for /summary/<id>, /summary/all, /calendar/<id>, and /calendar/all.
+# The all calendar just shows how many of the habits were logged that day. 
+# Which brings us to the calendar situation... we need a month class, with a 
+# list of day classes, each day needs to only have a number (the date) and a 
+# value, which can either be a number or a boolean
 
 def index(request):
 	return HttpResponse("Hello butt")
