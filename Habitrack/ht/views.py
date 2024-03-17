06ed8e82034cede1ae56d4a5df92ffabe07bad90
Braden <bradenoptimus@gmail.com>
2024-03-16 21:17:31 -0700

    I've decided to add in summaries

--- Habitrack/ht/views.py
+++ Habitrack/ht/views.py
@@ -106,4 +106,21 @@ def summary(request, sum_id):
 	return HttpResponse("summary %s" % sum_id)
 
 def summaryAll(request):
-	return HttpResponse("summary all")
+	template = loader.get_template("ht/summary.html")
+        
+    # basically we want to make a list of habit objects, where each habit object 
+    # has the name of the habit (easy), the goal of the habit (easy), the 
+    # current rate of success/status, a list of the values, the days needed for 
+    # to meet the goal, days in, days left, 
+    # Since this 
+	thing = {
+        "vals": [0,1,0,1],
+        "name": "hello",
+        "curr": 0.7,
+        "goal": 0.9,
+        "days in": 3,
+        "days to go": 6,
+        "days needed": 4,
+        "other": "thing"
+    }
+	return HttpResponse(template.render({ "things": "thang" }, request))
